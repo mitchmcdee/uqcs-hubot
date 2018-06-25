65450bca4c20bf2c087f4419e69f0b754a22687a
mitchmcdee <mitchmcdee@gmail.com>
2018-06-25 20:30:03 +1000

    wavie: cleaned up wavie code

--- uqcsbot/scripts/wavie.py
+++ uqcsbot/scripts/wavie.py
@@ -2,14 +2,10 @@ from uqcsbot import bot
 import logging
 
 
-logger = logging.getLogger(__name__)
-
-
 @bot.on('message')
-def wave(evt):
+def wavie(evt):
     """
     :wave: reacts to "person joined/left this channel"
-
     @no_help
     """
     if evt.get('subtype') not in ['channel_join', 'channel_leave']:
@@ -17,10 +13,4 @@ def wave(evt):
     chan = bot.channels.get(evt['channel'])
     if chan is not None and chan.name == 'announcements':
         return
-    result = bot.api.reactions.add(
-        name='wave',
-        channel=chan.id,
-        timestamp=evt['ts'],
-    )
-    if not result.get('ok'):
-        logger.error(f"Error adding reaction: {result}")
+    bot.api.reactions.add(name='wave', channel=chan.id, timestamp=evt['ts'])
